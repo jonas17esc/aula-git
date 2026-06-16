94f5085f5950ebe977eeaeb05c4a415c99e47dff
duartesilvjonashenry <duartesilvjonashenry@gmail.com>
2026-06-16 17:53:13 +0000

    atividade 16/06 , objeto js

---
+++ objeto.js
@@ -0,0 +1,14 @@
+const pessoa = {
+    nome : "Alice",
+    idade : 19 ,
+    altura : 1.60 ,
+    sexo : "feminino",
+    peso : 60,
+    hobby : "ler "
+}
+console.log(pessoa.nome);
+console.log(pessoa.idade);
+console.log(pessoa.altura);
+console.log(pessoa.sexo);
+console.log(pessoa.peso);
+console.log(pessoa.hobby);
