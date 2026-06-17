dcb03a39fe7fc115681c715e1508629ff62df2c1
duartesilvjonashenry <duartesilvjonashenry@gmail.com>
2026-06-17 12:44:49 +0000

    Atividade função obj , 17/06

--- objfuncao.js
+++ objfuncao.js
@@ -1,8 +1,25 @@
 const calculadora = {
-    numero1 : 25,
-    numero2 : 25,
-    multiplicar(){
-        return this.numero1 * this.numero2
+    numero1: 25,
+    numero2: 25,
+
+    somar() {
+        return this.numero1 + this.numero2;
+    },
+
+    subtrair() {
+        return this.numero1 - this.numero2;
+    },
+
+    multiplicar() {
+        return this.numero1 * this.numero2;
+    },
+
+    dividir() {
+        return this.numero1 / this.numero2;
     }
 }
-console.log(calculadora.multiplicar())
+
+console.log(calculadora.somar());
+console.log(calculadora.subtrair());
+console.log(calculadora.multiplicar());
+console.log(calculadora.dividir());
