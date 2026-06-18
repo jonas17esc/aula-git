9318e0685b0059f3192432c841f43b7264aab8cb
duartesilvjonashenry <duartesilvjonashenry@gmail.com>
2026-06-18 17:06:15 +0000

    produtos 18/06 , objetos

---
+++ produtos.js
@@ -0,0 +1,13 @@
+const produtos = {
+    nome: "moto",
+    marca: "Yamaha",
+    valor: 18000,
+    comprar: "Vou sim"
+}
+
+for (let chave in produtos) {
+    console.log(`${chave}: ${produtos[chave]}`);
+}
+
+console.log(Object.keys(produtos));
+
